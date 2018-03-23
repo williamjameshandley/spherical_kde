c4cddc3c2ef5de69d50c498a24b3858237d0e461
Will Handley <wh260@cam.ac.uk>
2018-03-23 16:29:32 +0000

    Better Mollweide plot

--- main.py
+++ main.py
@@ -1,42 +1,31 @@
 import matplotlib
 import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
 from scipy.special import logsumexp
 from spherical_kde.distributions import VMF
-from spherical_kde.utils import cartesian_from_spherical
+from spherical_kde.utils import polar_to_decra, decra_to_polar
 
 %matplotlib
 from scipy.stats import ortho_group
 import numpy
 
+nsamples = 100
+theta_samples = 1+numpy.random.normal(size=nsamples)*0.3
+phi_samples = 1+numpy.random.normal(size=nsamples)*0.3 
+def kde(phi, theta, sigma):
+    return logsumexp(VMF(phi, theta, phi_samples, theta_samples, sigma),axis=-1) - numpy.log(len(theta_samples))
 
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='mollweide')
 
-theta = numpy.linspace(0, numpy.pi, 100)
-phi = numpy.linspace(0, 2*numpy.pi, 100)
-theta, phi = numpy.meshgrid(theta, phi)
+dec_samples, ra_samples = polar_to_decra(theta_samples, phi_samples)
+ax.plot(ra_samples, dec_samples, 'k.')
 
+ra = numpy.linspace(-numpy.pi, numpy.pi, 100)
+dec = numpy.linspace(-numpy.pi/2, numpy.pi/2, 100)
+X, Y = numpy.meshgrid(ra, dec)
 
-#g = ortho_group.rvs(dim=3)
-#k = KentDistribution()
-#fcolors = numpy.exp(k(theta, phi, 100, 0.00, *g))
+phi, theta = decra_to_polar(dec, ra)
+P = numpy.exp(kde(phi, theta, 0.2)) 
 
+ax.contour(X, Y, P, 3, colors='k')
 
-
-
-nsamples = 1000
-theta_samples = numpy.random.uniform(low=numpy.pi/4,high=3*numpy.pi/4,size=nsamples)
-phi_samples = numpy.random.uniform(low=0,high=numpy.pi/2,size=nsamples)
-def kde(theta, phi, sigma):
-    return logsumexp(VMF(theta, phi, theta_samples, phi_samples, sigma),axis=-1) - numpy.log(len(theta_samples))
-
-#ax.plot(x,y,z,'k.')
-
-fcolors = numpy.exp(kde(theta, phi, 0.05))
-fmax, fmin = fcolors.max(), fcolors.min()
-fcolors = (fcolors - fmin)/(fmax - fmin)
-
-x, y, z = cartesian_from_spherical(theta, phi)
-
-fig = plt.figure(figsize=plt.figaspect(1.))
-ax = fig.add_subplot(111, projection='3d')
-ax.plot_surface(x, y, z,  rstride=1, cstride=1, facecolors=matplotlib.cm.seismic(fcolors))
